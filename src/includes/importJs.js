7147d42307dff38cde43e6277507a244b9f69f92
RokeAlvo <RokeAlvo@gmail.com>
2019-05-20 00:59:31 +0700

    добавлен календарь

--- src/includes/importJs.js
+++ src/includes/importJs.js
@@ -1,5 +1,7 @@
 import "../blocks/button-like/button-like.js";
+import "../blocks/datepicker/datepicker.js";
 import "../blocks/dropdown/dropdown.js";
 import "../blocks/expand-checkbox-list/expand-checkbox-list.js";
+import "../blocks/form-select/form-select.js";
 import "../blocks/range-slider/range-slider.js";
 import "../blocks/SimpleStarRating/SimpleStarRating.js";
