9fa928fe875550663caac6a1ab5941acfa03c897
RokeAlvo <RokeAlvo@gmail.com>
2019-05-26 14:07:39 +0700

    мелкие правки кнопок

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import './main.scss';
-import './includes/importJs'
 
 const jquery = require("jquery");
 const pgwSlider = require("pgwSlider");
 window.$ = window.jQuery = jquery; 
 require("jquery-ui");
+
+import './includes/importJs'
+
 $(document).ready(function() {
-    $('.pgwSlider').pgwSlider({displayList: false, intervalDuration: 2000, transitionDuration: 1000});
+    $('.pgwSlider').pgwSlider({displayList: false, intervalDuration: 3000, transitionDuration: 3000});
+     // FIXME поправить единицы изменения в pgwSlider: если меняются размеры вюпорта после запуска страницы, скрипту пофиг.
 });
